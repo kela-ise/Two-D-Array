9fc484bf2ad60869a0afb66794ad7327dc8782af
Kela <kelaise2@gmail.com>
2025-02-09 16:51:39 -0500

    Get rows & col from user, to initialize the array

--- Two D Array Game/Program.cs
+++ Two D Array Game/Program.cs
@@ -5,7 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("2D Array");
-           
+            Console.Write("Enter # of Rows: ");
+            int enterRows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter # of Cols: ");
+            int enterCol = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your Array has {enterRows} rows & {enterCol} cols");
+            int[,] playGround = new int[enterRows, enterCol];
+
+
         }
     }
 }
