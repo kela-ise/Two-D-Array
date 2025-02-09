533b1215311ba1663a24e8efa8914d08762eb48c
Kela <kelaise2@gmail.com>
2025-02-09 18:32:12 -0500

    created an array of random symbols to update playground

--- Two D Array Game/Program.cs
+++ Two D Array Game/Program.cs
@@ -4,14 +4,35 @@
     {
         static void Main(string[] args)
         {
+            const int PLAYGROUND_INDEX = 0;
+            Random random = new Random();
+            char[] randomSymbols = { '%','*', '+', '@', '&', '#', '$', '!', '?' };// characters that will be randomly placed in the grid/array
             Console.WriteLine("2D Array");
             Console.Write("Enter # of Rows: ");
             int enterRows = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter # of Cols: ");
-            int enterCol = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Your Array has {enterRows} rows & {enterCol} cols");
-            int[,] playGround = new int[enterRows, enterCol];
+            int enterCol = Convert.ToInt32(Console.ReadLine());// using char because of randomSymbols
+            char[,] playGround = new char[enterRows, enterCol];
+            Console.WriteLine($"Your Array has {enterRows} rows & {enterCol} cols"); // 2d array
+            
+            
+             // iterate through i & j and assign a random symbol
+                for (int i = PLAYGROUND_INDEX; i < enterRows; i++)
+                {
+                    for (int j = PLAYGROUND_INDEX; j < enterCol; j++)
+                    {
+                        playGround[i,j] = randomSymbols[random.Next(randomSymbols.Length)];
 
+                    // print all the cells in the array
+                    for (int k = PLAYGROUND_INDEX; k  < enterCol ; k++)
+                    {
+                        Console.WriteLine(playGround[i, j]);
+                    }
+                    }
+                   
+                }
+               
+            
 
         }
     }
