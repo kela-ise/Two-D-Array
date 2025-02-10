97bc12dea9cde1d3b815cb1c5d11a5bc7b8852a3
Kela <kelaise2@gmail.com>
2025-02-09 19:51:38 -0500

    Update the forloop that prints random symbols

--- Two D Array Game/Program.cs
+++ Two D Array Game/Program.cs
@@ -6,7 +6,7 @@
         {
             const int PLAYGROUND_INDEX = 0;
             Random random = new Random();
-            char[] randomSymbols = { '%','*', '+', '@', '&', '#', '$', '!', '?' };// characters that will be randomly placed in the grid/array
+            char[] randomSymbols = { '%', '*', '+', '@', '&', '#', '$', '!', '?' };// characters that will be randomly placed in the grid/array
             Console.WriteLine("2D Array");
             Console.Write("Enter # of Rows: ");
             int enterRows = Convert.ToInt32(Console.ReadLine());
@@ -14,25 +14,18 @@
             int enterCol = Convert.ToInt32(Console.ReadLine());// using char because of randomSymbols
             char[,] playGround = new char[enterRows, enterCol];
             Console.WriteLine($"Your Array has {enterRows} rows & {enterCol} cols"); // 2d array
-            
-            
-             // iterate through i & j and assign a random symbol
-                for (int i = PLAYGROUND_INDEX; i < enterRows; i++)
-                {
-                    for (int j = PLAYGROUND_INDEX; j < enterCol; j++)
-                    {
-                        playGround[i,j] = randomSymbols[random.Next(randomSymbols.Length)];
 
-                    // print all the cells in the array
-                    for (int k = PLAYGROUND_INDEX; k  < enterCol ; k++)
-                    {
-                        Console.WriteLine(playGround[i, j]);
-                    }
-                    }
-                   
+            // iterate through i & j to assign & print random symbolsS 
+            for (int i = PLAYGROUND_INDEX; i < enterRows; i++)
+            {
+                for (int j = PLAYGROUND_INDEX; j < enterCol; j++)
+                {
+                    playGround[i, j] = randomSymbols[random.Next(randomSymbols.Length)];
+
+                    Console.WriteLine(playGround[i, j]);
                 }
-               
-            
+                Console.WriteLine();
+            }
 
         }
     }
