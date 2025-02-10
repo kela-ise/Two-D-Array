d2568fb85bb25a6df9802a5b569ebf740d82384e
Kela <kelaise2@gmail.com>
2025-02-10 03:44:16 -0500

    Removing const & changing variable names

--- Two D Array Game/Program.cs
+++ Two D Array Game/Program.cs
@@ -1,28 +1,29 @@
 ﻿namespace Two_D_Array_Game
 {
+   
+ 
     internal class Program
     {
         static void Main(string[] args)
         {
-            const int PLAYGROUND_INDEX = 0;
             Random random = new Random();
             char[] randomSymbols = { '%', '*', '+', '@', '&', '#', '$', '!', '?' };// characters that will be randomly placed in the grid/array
             Console.WriteLine("2D Array");
             Console.Write("Enter # of Rows: ");
-            int enterRows = Convert.ToInt32(Console.ReadLine());
+            int numberOfRows = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter # of Cols: ");
-            int enterCol = Convert.ToInt32(Console.ReadLine());// using char because of randomSymbols
-            char[,] playGround = new char[enterRows, enterCol];
-            Console.WriteLine($"Your Array has {enterRows} rows & {enterCol} cols"); // 2d array
+            int noOfColumns = Convert.ToInt32(Console.ReadLine());
+            char[,] playGround = new char[numberOfRows, noOfColumns];// using char because of randomSymbols
+            Console.WriteLine($"Your Array has {numberOfRows} rows & {noOfColumns} cols"); // 2d array
 
-            // iterate through i & j to assign & print random symbolsS 
-            for (int i = PLAYGROUND_INDEX; i < enterRows; i++)
+            // iterate through rows & columns to assign & print random symbolsS 
+            for (int rows = 0; rows < numberOfRows; rows++)
             {
-                for (int j = PLAYGROUND_INDEX; j < enterCol; j++)
+                for (int cols = 0; cols < noOfColumns; cols++)
                 {
-                    playGround[i, j] = randomSymbols[random.Next(randomSymbols.Length)];
-
-                    Console.Write($"Index [{i}{j}]= {playGround[i , j]}      ");
+                    playGround[rows, cols] = randomSymbols[random.Next(randomSymbols.Length)];
+                    Console.Write(playGround[rows, cols]+ "  \t");
+                    //Console.Write($"Index [{i}*{j}]= {playGround[i , j]}      ");
                 }
                 Console.WriteLine();
             }
