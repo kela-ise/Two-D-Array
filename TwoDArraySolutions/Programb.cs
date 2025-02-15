995269c5d931944993e9cd37e086850abb4c8d45
Kela <kelaise2@gmail.com>
2025-02-15 16:46:22 -0500

    Add 2nd display mode

--- TwoDArraySolutions/Programb.cs
+++ TwoDArraySolutions/Programb.cs
@@ -17,24 +17,47 @@ namespace TwoDArraySolutions
             int noOfColumns = Convert.ToInt32(Console.ReadLine());
             char[,] playGround = new char[numberOfRows, noOfColumns];// using char because of randomSymbols
             Console.WriteLine($"Your Array has {numberOfRows} rows & {noOfColumns} cols"); // 2d array
-            Console.WriteLine("+----+----+----+----+", Color.Orange); //top border
-            for (int rows = 0; rows < numberOfRows; rows++) // iterate through rows & columns to assign & print random symbolsS 
+
+
+            Console.Write("Pick a display mode by entering 1 or 2:  ");
+            int displayMode = Convert.ToInt32(Console.ReadLine());
+            if (displayMode == 1)
             {
-               
-
-                for (int cols = 0; cols < noOfColumns; cols++)
+                Console.WriteLine("+----+----+----+----+", Color.Orange); //top border
+                for (int rows = 0; rows < numberOfRows; rows++) // iterate through rows & columns to assign & print random symbolsS 
                 {
-                    Console.Write("|", Color.Red); // left border
-                    playGround[rows, cols] = randomSymbols[random.Next(randomSymbols.Length)];
-                    //Console.Write(" 0 ", Color.Pink);
-                    Console.Write("  " + playGround[(rows), (cols)] + "  ", Color.Bisque);
-                    //Console.Write($"Index [{i}*{j}]= {playGround[i , j]}      ");
 
+
+                    for (int cols = 0; cols < noOfColumns; cols++)
+                    {
+                        Console.Write("|", Color.Red); // left border
+                        playGround[rows, cols] = randomSymbols[random.Next(randomSymbols.Length)];
+                        //Console.Write(" 0 ", Color.Pink);
+                        Console.Write("  " + playGround[(rows), (cols)] + "  ", Color.Bisque);
+                        //Console.Write($"Index [{i}*{j}]= {playGround[i , j]}      ");
+
+                    }
+                    Console.WriteLine("|", Color.Red); // right border
+                    Console.WriteLine("+----+----+----+----+", Color.Orange); // bottom border
                 }
-                Console.WriteLine("|", Color.Red); // right border
-                Console.WriteLine("+----+----+----+----+", Color.Orange); // bottom border
             }
-           
+
+            else if (displayMode == 2)
+            {
+                for (int rows = 0; rows < numberOfRows; rows++) // iterate through rows & columns to assign & print random symbolsS 
+                {
+
+
+                    for (int cols = 0; cols < noOfColumns; cols++)
+                    {
+                        playGround[rows, cols] = randomSymbols[random.Next(randomSymbols.Length)];
+                        Console.Write(playGround[(rows), (cols)]);
+
+                    }
+                    Console.WriteLine("");
+                }
+            }
+
+            }
         }
     }
-}
